e6c2b7a3d5b1a21ad049b5332440113d179780ab
Jose Osuna <2281302+joseosuna-engineer@users.noreply.github.com>
2020-11-21 19:19:25 -0300

    fixes sonarcloud: Source files should not have any duplicated blocks

--- src/main/java/xyz/wiraharas/controller/Controller.java
+++ src/main/java/xyz/wiraharas/controller/Controller.java
@@ -1,6 +1,5 @@
 package xyz.wiraharas.controller;
 
-import java.util.List;
 import xyz.wiraharas.dto.CuentaDto;
 import xyz.wiraharas.service.CuentaService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,9 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import xyz.wiraharas.dto.TitularDto;
-import xyz.wiraharas.dto.UserDto;
 import xyz.wiraharas.service.TitularService;
-import xyz.wiraharas.service.UserService;
 
 @RestController
 class Controller {
@@ -20,9 +17,6 @@ class Controller {
 
     @Autowired
     private TitularService titularService;
-
-    @Autowired
-    private UserService userService;
 
     @GetMapping("/cuenta/{guid}")
     public CuentaDto getCuenta(@PathVariable String guid) {
@@ -34,12 +28,6 @@ class Controller {
     public TitularDto getTitular(@PathVariable String guid) {
 
         return titularService.get(guid);
-    }
-
-    @GetMapping("/usuario")
-    public List<UserDto> listAllUsers() {
-
-        return userService.listAll();
     }
 
 }
